0cf9c94e43b1dad60d51fffde8197c9a9e6f4fe8
mfariza2111 <farizamuhammad310@gmail.com>
2021-10-09 15:58:47 +0700

    Add project files.

---
+++ ClientWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ClientWeb.WebWCF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        FirstWCFClient client = new FirstWCFClient();
+        public ActionResult Index()
+        {
+            ViewBag.Message = client.Message();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
